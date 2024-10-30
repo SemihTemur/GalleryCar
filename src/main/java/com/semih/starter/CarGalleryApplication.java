b1a4e48c73310034b53e9082c07f18f38b5e4804
semih <sinematv44@gmail.com>
2024-10-31 00:44:35 +0300

    initial-commit

---
+++ src/main/java/com/semih/starter/CarGalleryApplication.java
@@ -0,0 +1,13 @@
+package com.semih.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarGalleryApplication.class, args);
+	}
+
+}
